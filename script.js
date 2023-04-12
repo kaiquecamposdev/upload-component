fbe3174e498e3fee07d98bd85e94947b4883cd43
Kaique <kaiquedecampos2@gmail.com>
2023-04-12 19:22:42 -0300

    creating project

---
+++ script.js
@@ -0,0 +1,14 @@
+const dropArea = document.querySelector('.drag-files')
+
+const mouseEvents = () => {
+
+// cursor events
+dropArea.addEventListener('mouseenter', () => dropArea.classList.add('dragover'));
+
+// drags events
+dropArea.addEventListener('dragover', () => dropArea.classList.add('dragover'));
+dropArea.addEventListener('dragleave', () => dropArea.classList.remove('dragover'));
+
+}
+mouseEvents()
+
